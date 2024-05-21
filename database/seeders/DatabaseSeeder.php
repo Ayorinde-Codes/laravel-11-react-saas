fccac4245e9bc9197f21b1574d85ca227d68ed67
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2024-05-21 23:45:56 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Feature;
+use App\Models\Package;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'ayorinde',
+            'email' => 'ayorinde@example.com',
+            'password' => bcrypt('123.password'),
+        ]);
+
+        Feature::create([
+            'image' => 'https://static-00.iconduck.com/assets.00/plus-icon-2048x2048-z6v59bd6.png',
+            'route_name' => 'feature1.index',
+            'name' => 'Calculate Sum',
+            'description' => 'calculate sum of two numbers',
+            'required_credits' => 1,
+            'active' => true
+        ]);
+
+        Feature::create([
+            'image' => 'https://cdn-icons-png.freepik.com/512/929/929430.png',
+            'route_name' => 'feature2.index',
+            'name' => 'Calculate Difference',
+            'description' => 'calculate difference of two numbers',
+            'required_credits' => 3,
+            'active' => true
+        ]);
+
+        Package::create([
+            'name' => 'Basic',
+            'price' => 5,
+            'credits' => 20
+        ]);
+        Package::create([
+            'name' => 'Silver',
+            'price' => 20,
+            'credits' => 100
+        ]);
+        Package::create([
+            'name' => 'Gold',
+            'price' => 50,
+            'credits' => 500
+        ]);
+    }
+}
